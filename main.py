bf590a091d13c6f70dceab6303c5d48d638aba44
Abhishek Jaiswal <61107453+Abhishekjl@users.noreply.github.com>
2021-08-15 02:20:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from os.path import isfile, join
 
 import transformers
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
-model_name = "deepset/roberta-base-squad2"
+model_name = "distilbert-base-uncased-distilled-squad"
 model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
